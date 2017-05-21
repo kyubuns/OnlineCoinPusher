ad0f029bd4e0a8950185e5e01671b677a48e645b
kyubuns <kyubuns@gmail.com>
2017-05-21 13:40:58 +0900

    コイン増える

---
+++ Assets/Scripts/CoinPusher/CoinCatcher.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TrueSync;
+
+namespace CoinPusher
+{
+	public class CoinCatcher : TrueSyncBehaviour
+	{
+		public void OnSyncedTriggerEnter(TSCollision other)
+		{
+			if (other.gameObject.tag != "Coin") return;
+			foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+			{
+				player.GetComponent<CoinSpawner>().Coin++;
+			}
+			TrueSyncManager.SyncedDestroy(other.gameObject);
+		}
+	}
+}
